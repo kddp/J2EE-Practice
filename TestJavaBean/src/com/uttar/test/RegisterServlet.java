3ffcb2f0109ad9377aa09bc80d50841b289e59d1
abhisheknag <nag.abhi2006@gmail.com>
2015-04-20 17:59:06 +0530

    Test Java beans

---
+++ TestJavaBean/src/com/uttar/test/RegisterServlet.java
@@ -0,0 +1,46 @@
+package com.uttar.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.uttara.test.RegisterBean;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		RegisterBean rb = (RegisterBean)request.getAttribute("reg");
+		String name = rb.getUname();
+		String email = rb.getEmail();
+		String pwd = rb.getPwd();
+		out.print(name+","+email+","+pwd);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ TestJavaBean/src/com/uttara/test/RegisterBean.java
@@ -0,0 +1,31 @@
+package com.uttara.test;
+
+public class RegisterBean 
+{
+	String uname,email,pwd;
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+}
